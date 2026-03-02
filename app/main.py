7013dfd2ecb06a46210d53c7b132422a7def88e7
neda <neda.n.kovacevic@gmail.com>
2026-03-02 19:26:29 +0100

    cors dodat

--- app/main.py
+++ app/main.py
@@ -6,9 +6,21 @@ from app.api.routes.stanje import router as stanje_router
 from app.api.routes.iothub import router as iothub_router
 from app.api.routes.devices import router as devices_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Recycling Backend")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5177",
+        "https://frontendreciklomat.z1.web.core.windows.net/"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(otpad_router)
@@ -18,14 +30,11 @@ app.include_router(stanje_router)
 app.include_router(iothub_router)
 app.include_router(devices_router)
 
-from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],          # MVP: svuda dozvoli (posle zakljucas na domen webapp-a)
-    allow_methods=["*"],
-    allow_headers=["*"], # treba *
-)
+
+
+
+
 @app.get("/")
 def root():
     return {"service": "reciklomat-backend", "ok": True}
